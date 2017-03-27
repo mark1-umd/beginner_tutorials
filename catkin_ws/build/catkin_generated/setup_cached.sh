3c502e5e6a6303953af2b94f7b16b8bbdf0671ec
Mark Jenkins <mark1@umd.edu>
2017-03-27 15:55:25 -0700

    Starting ROS PublisherSubscriber tutorial - Initialized Eclipse

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -5,8 +5,16 @@
 # it emulates the modifications of the setup script without recurring computations
 
 # new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/viki/ROS/beginner_tutorials/catkin_ws/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/viki/ROS/beginner_tutorials/catkin_ws/build/test_results"
 
 # modified environment variables
-export LD_LIBRARY_PATH="/home/viki/ROS/beginner_tutorials/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/viki/ROS/beginner_tutorials/catkin_ws/devel/lib:/opt/ros/indigo/lib"
-export PKG_CONFIG_PATH="/home/viki/ROS/beginner_tutorials/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/viki/ROS/beginner_tutorials/catkin_ws/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export CMAKE_PREFIX_PATH="/home/viki/ROS/beginner_tutorials/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/viki/ROS/beginner_tutorials/catkin_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/viki/ROS/beginner_tutorials/catkin_ws/devel/lib:/home/viki/ROS/beginner_tutorials/catkin_ws/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/viki/ROS/beginner_tutorials/catkin_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/viki/ROS/beginner_tutorials/catkin_ws/devel/lib/pkgconfig:/home/viki/ROS/beginner_tutorials/catkin_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
 export PWD="/home/viki/ROS/beginner_tutorials/catkin_ws/build"
+export PYTHONPATH="/home/viki/ROS/beginner_tutorials/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/viki/ROS/beginner_tutorials/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/viki/ROS/beginner_tutorials/catkin_ws/src:$ROS_PACKAGE_PATH"

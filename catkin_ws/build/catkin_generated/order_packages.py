d88533d9b8dee18c67f6521e6ca8e27a3d3779e8
Mark Jenkins <mark1@umd.edu>
2017-03-27 12:29:41 -0700

    Completed ROS Creating Packages tutorial

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = "/home/viki/ROS/beginner_tutorials/catkin_ws/src"
+whitelisted_packages = "".split(';') if "" != "" else []
+blacklisted_packages = "".split(';') if "" != "" else []
+underlay_workspaces = "/home/viki/ROS/beginner_tutorials/catkin_ws/devel;/opt/ros/indigo".split(';') if "/home/viki/ROS/beginner_tutorials/catkin_ws/devel;/opt/ros/indigo" != "" else []
